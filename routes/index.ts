dc7c5d312b883942b40683188dbfa577482c056f
Maninder Pahal <mpahal123@gmail.com>
2024-02-06 19:39:13 -0800

    setup route endpoints

--- routes/index.ts
+++ routes/index.ts
@@ -7,4 +7,24 @@ router.get('/', function(req: Request , res: Response, next) {
   res.render('index', { title: 'wattup pussy' });
 });
 
+router.post('/sign-up', function(req: Request , res: Response, next) {
+  res.render('index', { title: 'signup route' });
+});
+
+router.post('/log-in', function(req: Request , res: Response, next) {
+  res.render('index', { title: 'login route' });
+});
+
+router.post('/log-out', function(req: Request , res: Response, next) {
+  res.render('index', { title: 'wattup pussy' });
+});
+
+router.post('/send', function(req: Request , res: Response, next) {
+  res.render('index', { title: 'send message route' });
+});
+
+router.get('/profile/:id', function(req: Request , res: Response, next) {
+  res.render('index', { title: 'user profile' });
+});
+
 export default router;
