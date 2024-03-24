bca16427cc85d49555b0f3fcd012136806f3d159
Maninder Pahal <mpahal123@gmail.com>
2024-03-23 21:36:04 -0700

    updated msg model

--- models/MessagesSchema.sql
+++ models/MessagesSchema.sql
@@ -2,10 +2,10 @@ USE chatter;
 
 CREATE TABLE messages (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    sender_id INT,
-    receiver_id INT,
     content TEXT,
+    room_id INT,
     timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (sender_id) REFERENCES users(id),
-    FOREIGN KEY (receiver_id) REFERENCES users(id)
+    FOREIGN KEY (sender_id) REFERENCES user(id),
+    FOREIGN KEY (receiver_id) REFERENCES user(id),
+    FOREIGN KEY (room_id) REFERENCES room(id)
 );
