2d921b0f6a64222c7773a4b7031a3d2dc40fabe0
Maninder Pahal <mpahal123@gmail.com>
2024-05-15 18:20:55 -0700

    added passport jwt authentication middleware to all routes except login and signup

--- routes/index.ts
+++ routes/index.ts
@@ -5,34 +5,30 @@ import { getChats, log_in, logout, sign_up, userProfile } from "../controllers/u
 import { create_room, get_rooms, recent_room } from "../controllers/roomController.js";
 import { all_users } from "../controllers/userController.js";
 import { delete_msg, get_message, update_msg } from "../controllers/messagesController.js";
-
-/* GET home page. */
-router.get('/', function(req: Request , res: Response, next) {
-  res.render('index', { title: 'wattup pussy' });
-});
+import passport from "passport";
 
 router.post('/sign-up', sign_up);
 
 router.post('/log-in', log_in);
 
-router.post('/log-out', logout);
+router.post('/log-out', passport.authenticate('jwt', {session: false}), logout);
 
-router.get('/profile/:id', userProfile);
+router.get('/profile/:id', passport.authenticate('jwt', {session: false}), userProfile);
 
-router.get('/users', all_users)
+router.get('/users', passport.authenticate('jwt', {session: false}), all_users)
 
-router.get('/user/:id/chats', getChats)
+router.get('/user/:id/chats', passport.authenticate('jwt', {session: false}), getChats)
 
-router.post('/create-room', create_room)
+router.post('/create-room', passport.authenticate('jwt', {session: false}), create_room)
 
-router.get('/rooms/:id', get_rooms)
+router.get('/rooms/:id', passport.authenticate('jwt', {session: false}), get_rooms)
 
-router.get('/get-messages/:roomId', get_message)
+router.get('/get-messages/:roomId', passport.authenticate('jwt', {session: false}), get_message)
 
-router.post('/update-message', update_msg)
+router.post('/update-message', passport.authenticate('jwt', {session: false}), update_msg)
 
-router.post('/delete-message', delete_msg)
+router.post('/delete-message', passport.authenticate('jwt', {session: false}), delete_msg)
 
-router.get('/last-room', recent_room)
+router.get('/last-room', passport.authenticate('jwt', {session: false}), recent_room)
 
 export default router;
